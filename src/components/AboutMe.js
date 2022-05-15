80b4c672d321e89f4c8e528f39b5f860da46dfc2
Harris <harrisbchong@gmail.com>
2022-05-15 00:13:04 -0400

    added temp about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './HeroSection.css';
-import '../App.css';
+import './AboutMe.css';
 
 function AboutMe() {
     return (
-        <div className='about-me'>
-            <h1>All about me</h1>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
+        /* introduction
+        location */
+        <div className='section'>
+            <h1>
+                "INTRODUCTION"
+            </h1>
+            <p className='paragraph'>
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
+            </p>
+            <h1>
+                "EXPERIENCES"
+            </h1>
+            <p className='paragraph'>
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+            </p>
+            <h1>
+                "HOBBIES"
+            </h1>
+            <p className='paragraph'>
+                In my free time I like to exercise and play sports like basketball, volleyball, and frisbee.
+                When I'm not outside, I like to play videogames, sometimes play piano, and work on coding side projects
+                to learn about different technologies.
+
+            </p>
 
 
         </div>
